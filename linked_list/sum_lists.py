44f6be44fed54eddf27443a9ded97a94369c39ef
Ben Arbib <ba2490@columbia.edu>
2018-06-30 18:09:25 -0500

    add recursive option

--- linked_list/sum_lists.py
+++ linked_list/sum_lists.py
@@ -35,6 +35,30 @@ def sum_lists(l1, l2):
     
     return head
 
+def sum_lists_recursive(l1, l2):
+    return sum_list_helper(l1, l2, 0)
+
+def sum_list_helper(l1, l2, borrow):
+    if not l1 and not l2 and borrow == 0:
+        return None
+
+    sum_elems = borrow + (0 if not l1 else l1.val)
+    sum_elems += 0 if not l2 else l2.val
+    
+    if sum_elems > 9:
+        borrow = 1
+        sum_elems -=10
+    else:
+        borrow = 0
+
+    l1 = None if not l1 else l1.next
+    l2 = None if not l2 else l2.next
+
+    node = Node(sum_elems)
+    node.next = sum_list_helper(l1, l2, borrow) 
+
+    return node
+
 def reverse_list(l):
     new_list = None
     next_node = l.next
@@ -47,11 +71,15 @@ def reverse_list(l):
     
     return new_list
 
+
 first = LinkedList.random_linkedlist()
 second = LinkedList.random_linkedlist()
 first.print_linkedlist()
 second.print_linkedlist()
 
+print("iterative")
 sum_lists(first, second).print_linkedlist()
 
+print("revcursive")
+sum_lists_recursive(first, second).print_linkedlist()
 # for the FOLLOW UP - just run the lists in the reverse list funcion
