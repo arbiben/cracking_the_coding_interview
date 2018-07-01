6911ebdfc8ec09577453c6d1a2bdef6f709e92c7
Ben Arbib <ba2490@columbia.edu>
2018-07-01 18:45:58 -0500

    real queue via stacks and stack class added

---
+++ stacks_and_queues/Stack.py
@@ -0,0 +1,38 @@
+# this is a stack class implementation
+
+class Stack:
+    def __init__(self):
+        self.head = None
+        self.count = 0
+
+    def pop(self):
+        if not self.head:
+            raise ValueError('Stack underflow my dude')
+
+        node = self.head
+        self.head = self.head.next
+        self.count -= 1
+
+        return node
+
+    def push(self, val):
+        if not self.head:
+            self.head = Node(val)
+
+        else:
+            node = Node(val)
+            node.next = self.head
+            self.head = node
+        
+        self.count += 1
+
+    def peek(self):
+        return self.head
+            
+    def isEmpty(self):
+        return self.count == 0
+
+class Node:
+    def __init__(self, val):
+        self.next = None
+        self.val = val
---
+++ stacks_and_queues/queue_via_stacks.py
@@ -0,0 +1,23 @@
+# create a queue from two stacks
+from Stack import Stack
+
+class queue_from_stacks:
+    def __init__(self):
+        self.s1 = Stack()
+        self.s2 = Stack()
+
+    def enqueue(self, val):
+        self.s1.push(val)
+
+    def dequeue(self):
+        if not self.s2.isEmpty():
+            return self.s2.pop()
+        else:
+            while self.s1.peek():
+                self.s2.push(self.s1.pop().val)
+
+            if not self.s2.isEmpty():
+                return self.s2.pop()
+
+        raise ValueError('Queue is empty')
+
