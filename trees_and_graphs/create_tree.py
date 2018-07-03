08841c3bde12e20573386f0ea98b87d5d313f1ad
Ben Arbib <ba2490@columbia.edu>
2018-07-03 00:17:36 -0500

    create class that generates trees

---
+++ trees_and_graphs/create_tree.py
@@ -0,0 +1,55 @@
+from Node import Node
+import random
+
+class random_tree:
+    def __init__(self):
+        new_list = [i for i in range(10)]
+        self.balanced = self.list_to_tree(new_list)
+        self.unbal = self.unbalanced_tree_generator()
+
+    def list_to_tree(self, l):
+        return self.to_tree_helper(l, 0, len(l)-1)
+
+
+    def to_tree_helper(self, sorted_list, l, r):
+        if l > r:
+            return None
+        mid = int((l+r)/2)
+        node = Node(sorted_list[mid])
+        node.left = self.to_tree_helper(sorted_list, l, mid-1)
+        node.right = self.to_tree_helper(sorted_list, mid+1, r)
+
+        return node
+    
+    def unbalanced_tree_generator(self):
+        root = Node(1)
+        node = root
+        for _ in range(random.randint(5, 10)):
+            node.left = Node(random.randint(0,100))
+            node.right = Node(random.randint(0,100))
+            i = random.randint(0,1)
+            if i:
+                node = node.left
+            else:
+                node = node.right
+        
+        return root
+
+    def list_of_depth(self, root):
+        return self.list_of_depth_helper(root, 0, [])
+
+
+    def list_of_depth_helper(self, root, d, list_of_lists):
+        if not root:
+            return None
+
+        if len(list_of_lists) == d:
+            list_of_lists.append([root.val])
+        else:
+            list_of_lists[d].append(root.val)
+
+        self.list_of_depth_helper(root.left, d+1, list_of_lists)
+        self.list_of_depth_helper(root.right, d+1, list_of_lists)
+
+        return list_of_lists
+            
