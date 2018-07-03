160ae93f7aae68c5da7be15a71458faec321a6c6
Ben Arbib <ba2490@columbia.edu>
2018-07-03 00:34:25 -0500

    added another way to solve this problem

--- trees_and_graphs/is_balanced.py
+++ trees_and_graphs/is_balanced.py
@@ -3,25 +3,40 @@
 
 from Node import Node
 from create_tree import random_tree # generates trees
+import sys
 
-def is_balanced(head):
-    if not head:
+def is_balanced(root):
+    if not root:
         return True
 
-    return is_balanced_helper(head) > -1
+    return is_balanced_helper(root) > -1
 
-def is_balanced_helper(head):
-    if not head:
+def is_balanced_helper(root):
+    if not root:
         return 0
 
-    left = is_balanced_helper(head.left) + 1
-    right = is_balanced_helper(head.right) + 1
+    left = is_balanced_helper(root.left) + 1
+    right = is_balanced_helper(root.right) + 1
 
     if right == 0 or left == 0 or abs(left-right) > 1:
         return -1
 
     return max(left, right)
 
+def is_balanced_better(root):
+    if not root:
+        return -1
+
+    left = is_balanced_better(root.left)
+    right = is_balanced_better(root.right)
+    if left == -sys.maxsize or right == -sys.maxsize:
+        return -sys.maxsize
+
+    diff = abs(left - right)
+    if diff > 1:
+        return -sys.maxsize
+
+    return max(left, right) + 1
 
 t = random_tree()
 balanced = t.balanced
@@ -29,3 +44,6 @@ unbalanced = t.unbal
 
 print("Should return True: {}".format(is_balanced(balanced)))
 print("Should return False: {}".format(is_balanced(unbalanced)))
+
+print("Better version True: {}".format(-sys.maxsize != is_balanced_better(balanced)))
+print("Better version False: {}".format(-sys.maxsize != is_balanced_better(unbalanced)))
