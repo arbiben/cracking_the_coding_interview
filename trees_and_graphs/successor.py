1cf62a52fdcbfc6685518a7aea3344108beba09c
Ben Arbib <ba2490@columbia.edu>
2018-07-04 00:44:23 -0500

    get most left method added

--- trees_and_graphs/successor.py
+++ trees_and_graphs/successor.py
@@ -7,8 +7,14 @@ def successor(node):
     if not node:
         return None
     if node.right:
-        return node.right
+        return get_most_left(node.right)
     return successor_helper(node, node.parent)
+
+def get_most_left(node):
+    if node.left:
+        return get_most_left(node.left)
+    else:
+        return node
 
 def successor_helper(child, parent):
     if not parent:
@@ -27,8 +33,9 @@ five = tree.right.left
 six = five.right
 nine = tree.right.right.right
 
-print("Successor of 3 should be 4: {}".format(successor(three).val))
 print("Successor of 2 should be 3: {}".format(successor(two).val))
+print("Successor of 3 should be 4: {}".format(successor(three).val))
+print("Successor of 4 should be 5: {}".format(successor(tree).val))
 print("Successor of 5 should be 6: {}".format(successor(five).val))
 print("Successor of 6 should be 7: {}".format(successor(six).val))
 print("Successor of 9 should be None: {}".format(successor(nine)))
