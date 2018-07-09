97f4b4cc13e4615ae1370f31141b2f0dad9ccbdc
Ben Arbib <ba2490@columbia.edu>
2018-07-08 21:55:03 -0500

    modification to efficiency

--- bit_manipulation/binary_to_string.py
+++ bit_manipulation/binary_to_string.py
@@ -6,20 +6,38 @@ def binary_to_string(x):
         return "Error"
 
     decimal = get_decimal(x)
+    int_bin_val = get_int_binary(int(x))
     binary_val = '.'
-    while len(binary_val) < 33 and decimal != 1:
+    while len(binary_val) <= 33 and decimal > 0:
         decimal *= 2
         if decimal < 1:
             binary_val += '0'
         else:
             binary_val += '1'
-            if decimal > 1:
-                decimal = get_decimal(decimal)
+            decimal -= 1
+    
 
-    return binary_val
+    if len(binary_val) >= 34:
+        return "Error booooo"
+    
+    return str(int_bin_val) + str(binary_val)
 
 def get_decimal(x):
     full_num = str(x).split('.')
     return float(x) - float(full_num[0])
 
-print(binary_to_string(0.15625))
+def get_int_binary(x):
+    power = 0
+    while x >= 2**(power+1):
+        power += 1
+    bin_val = ''
+    for i in range(power, -1, -1):
+        if x >= 2**i:
+            bin_val += '1'
+            x -= (2**i)
+        else:
+            bin_val += '0'
+
+    return bin_val
+
+print(binary_to_string(40.15625))
