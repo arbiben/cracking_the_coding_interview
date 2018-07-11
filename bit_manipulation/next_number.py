90feb4bea1a77878613eedf1e8d6e9f7236f4e1b
Ben Arbib <ba2490@columbia.edu>
2018-07-10 23:48:41 -0500

    working on book solution

--- bit_manipulation/next_number.py
+++ bit_manipulation/next_number.py
@@ -6,21 +6,47 @@ def next_num(num):
     if not num:
         return None
     first_zero = get_first_zero(num, 0)
+    print("Swapping only one bit - not what the question asks")
     print("Number: {} \t\t\tbinary: {}".format(num, "{0:b}".format(num)))
     print_next_largest(num, first_zero)
     print_next_smallest(num, first_zero)
     print_largest(num, first_zero)
     print_smallest(num, first_zero)
+    print("\nThis is what the question asks")
+    print("Number: {} \t\t\tbinary: {}".format(num, "{0:b}".format(num)))
+    print_next_largest_number(num)
+    print_next_smallest_number(num)
+
+def print_next_largest_number(num):
+    first_one = get_first_non_trailing_one(num)
+    ones = count_ones_after_n(num, first_one) - 4
+    num = (1 << first_one) ^ num
+    
+    mask = (1 << first_one) -1
+    diff = first_one - ones
+    zero_mask = ~((1 << diff) - 1)
+    mask = mask & zero_mask
+    num = mask | num
+    print("next largest: {} \t\tbinary: {}".format(num, "{0:b}".format(num)))
+
+def print_next_smallest_number(num):
+    first_zero = get_first_non_trailing_zero(num)
+    ones = count_ones_after_n(num, first_zero) - 1
+    num = insert_one_and_mask(num, first_zero)
+    
+    one_mask = ((1 << ones) - 1)
+    num = num | one_mask
+    print("next smallest: {} \t\tbinary: {}".format(num, "{0:b}".format(num)))
 
 def print_largest(num, first_zero):
     first_one = get_first_one(num)
     largest = swap_bits(num, first_zero, first_one)
-    print("largest number: {} \t\tbinary: {}".format(largest, "{0:b}".format(largest)))
+    print("largerst swap: {} \t\tbinary: {}".format(largest, "{0:b}".format(largest)))
 
 def print_smallest(num, first_zero):
     last_one = get_last_one(num)
     smallest = swap_bits(num, first_zero, last_one)
-    print("smallest number: {} \t\tbinary: {}".format(smallest, "{0:b}".format(smallest)))
+    print("smallest swap: {} \t\tbinary: {}".format(smallest, "{0:b}".format(smallest)))
 
 def print_next_largest(num, first_zero):
     if first_zero == 0:
@@ -28,7 +54,7 @@ def print_next_largest(num, first_zero):
         first_zero = get_first_zero(num, first_one)
 
     next_largest = swap_bits(num, first_zero, first_zero-1)
-    print("next largest: {} \t\tbinary: {}".format(next_largest, "{0:b}".format(next_largest)))
+    print("one larger swap: {} \t\tbinary: {}".format(next_largest, "{0:b}".format(next_largest)))
 
 def print_next_smallest(num, first_zero):
     next_one = get_next_zero(num, first_zero)
@@ -36,7 +62,7 @@ def print_next_smallest(num, first_zero):
         print("no num smaller than {}".format(num))
         return
     next_smallest = swap_bits(num, first_zero, next_one)
-    print("next smallest: {} \t\tbinary: {}".format(next_smallest, "{0:b}".format(next_smallest)))
+    print("one swap smaller: {} \tbinary: {}".format(next_smallest, "{0:b}".format(next_smallest)))
 
 def swap_bits(num, zero, one):
     new_num = num | 1 << zero
@@ -69,4 +95,36 @@ def get_last_one(num):
         last_one -= 1
     return last_one
 
-next_num(27)
+def get_first_non_trailing_zero(num):
+    i = 0
+    while not 1 << i & num:
+        i += 1
+    while 1 << i & num:
+        i += 1
+    return i
+
+def get_first_non_trailing_one(num):
+    i = 0
+    while not 1 << i & num:
+        i += 1
+    while 1 << i & num:
+        i += 1
+    while not 1 << i & num:
+        i += 1
+    return i
+
+def insert_one_and_mask(num, first_zero):
+    num = num | 1 << first_zero
+    mask = ~((1 << first_zero) - 1)
+    num = mask & num
+    return num
+
+def count_ones_after_n(num, n):
+    count = 0
+    while n > -1:
+        if num & 1 << n:
+            count += 1
+        n -= 1
+    return count
+
+next_num(13948)
