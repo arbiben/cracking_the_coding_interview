0cd612a0950b523c12ee4e5e8ede2a340335b3f2
BenIBM <ba2490@columbia.edu>
2018-07-13 10:17:03 -0500

    small changes

--- recursion_and_dynamic_programming/robot_in_grid.py
+++ recursion_and_dynamic_programming/robot_in_grid.py
@@ -8,14 +8,12 @@ def plan_path(matrix, i, j):
     w = len(matrix[0])-1
     h = len(matrix)-1
     if i>h or j>w:
-        print("out of range!")
         return False
 
     if i==h and j==w:
         return ("I'm Home!!")
 
     if matrix[i][j] == -1:
-        print("BLOCK at {},{}".format(i, j))
         return False
 
     count+= 1
@@ -27,16 +25,13 @@ def plan_path_dy(matrix, i, j):
     w = len(matrix[0])-1
     h = len(matrix)-1
 
+    if i>h or j>w:
+        return False
+
     if matrix[i][j] == -2:
-        print("visited at {},{}".format(i, j))
         return False
 
     if matrix[i][j] == -1:
-        print("BLOCK at {},{}".format(i, j))
-        return False
-
-    if i>h or j>w:
-        print("out of range!")
         return False
 
     if i==h and j==w:
@@ -44,19 +39,22 @@ def plan_path_dy(matrix, i, j):
 
     matrix[i][j] = -2
     count += 1
-    return plan_path(matrix, i, j+1) or plan_path(matrix, i+1, j)
+    return plan_path_dy(matrix, i, j+1) or plan_path_dy(matrix, i+1, j)
 
 
 
 w, h = 8, 5
 matrix = [[0 for x in range(w)] for y in range(h)]
-matrix[0][2] = -1
+matrix[0][3] = -1
+matrix[1][3] = -1
+matrix[2][3] = -1
 matrix[2][1] = -1
-matrix[1][5] = -1
-matrix[2][5] = -1
-matrix[3][7] = -1
+matrix[2][2] = -1
+
+
+
 print(plan_path(matrix, 0,0))
-print(count)
+print("{} steps without dynamic programming".format(count))
 count = 0
 print(plan_path_dy(matrix, 0,0))
-print(count)
+print("{} steps with dynamic programming".format(count))
