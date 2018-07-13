544f9310ea4431f3412a1b941aa2db337767078c
Ben Arbib <ba2490@columbia.edu>
2018-07-12 23:40:13 -0500

    checked another option - it was ok, not amazing or something...

--- recursion_and_dynamic_programming/robot_in_grid.py
+++ recursion_and_dynamic_programming/robot_in_grid.py
@@ -1,11 +1,12 @@
 # a robot is sitting on the top left corner of a 2d array.
 # the robot can move right and down, some indices are "off limits"
 # design an algorithm to find a path to the bottom right
+count = 0
 
 def plan_path(matrix, i, j):
+    global count
     w = len(matrix[0])-1
     h = len(matrix)-1
-    print("{},{}".format(i, j))
     if i>h or j>w:
         print("out of range!")
         return False
@@ -17,6 +18,32 @@ def plan_path(matrix, i, j):
         print("BLOCK at {},{}".format(i, j))
         return False
 
+    count+= 1
+    return plan_path(matrix, i, j+1) or plan_path(matrix, i+1, j)
+
+# dynamic
+def plan_path_dy(matrix, i, j):
+    global count
+    w = len(matrix[0])-1
+    h = len(matrix)-1
+
+    if matrix[i][j] == -2:
+        print("visited at {},{}".format(i, j))
+        return False
+
+    if matrix[i][j] == -1:
+        print("BLOCK at {},{}".format(i, j))
+        return False
+
+    if i>h or j>w:
+        print("out of range!")
+        return False
+
+    if i==h and j==w:
+        return ("I'm Home!!")
+
+    matrix[i][j] = -2
+    count += 1
     return plan_path(matrix, i, j+1) or plan_path(matrix, i+1, j)
 
 
@@ -29,3 +56,7 @@ matrix[1][5] = -1
 matrix[2][5] = -1
 matrix[3][7] = -1
 print(plan_path(matrix, 0,0))
+print(count)
+count = 0
+print(plan_path_dy(matrix, 0,0))
+print(count)
