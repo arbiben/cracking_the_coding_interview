17b4f0368f9537952cada72abcf439d94d5dec63
Ben Arbib <ba2490@columbia.edu>
2018-07-12 21:45:52 -0500

    fib

--- recursion_and_dynamic_programming/fibonacci.py
+++ recursion_and_dynamic_programming/fibonacci.py
@@ -7,11 +7,11 @@ def fib(n):
     return fib_helper(n, [1,1])
 
 def fib_helper(n, l):
-    if len(l) > n-1:
-        return l[n-1]
+    if len(l) > n:
+        return l[n]
 
     next_num = fib_helper(n-1, l) + fib_helper(n-2, l)
     l.append(next_num)
     return next_num
 
-print(fib(8))
+print(fib(170))
