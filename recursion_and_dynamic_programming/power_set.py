6176a18dc4b650bfc4fc077bd8caedc241de0289
Ben Arbib <ba2490@columbia.edu>
2018-07-14 18:23:09 -0500

    added a slution with bit manipulation

--- recursion_and_dynamic_programming/power_set.py
+++ recursion_and_dynamic_programming/power_set.py
@@ -4,7 +4,12 @@ import copy
 def power_set(arr):
     if len(arr) == 0:
         return None
-    return get_power_set(arr, [], 0)
+    one = get_power_set(arr, [], 0)
+    one.sort()
+    two = get_power_set_binary(arr)
+    two.sort()
+    print("my way: {}".format(one))
+    print("binary: {}".format(two))
 
 def get_power_set(arr, subsets, i):
     if i == len(arr):
@@ -19,4 +24,22 @@ def get_power_set(arr, subsets, i):
     subsets.extend(arr_copy)
     return get_power_set(arr, subsets, i+1)
 
-print(power_set([1,2,3,4]))
+def get_power_set_binary(arr):
+    subsets = []
+    size = 1 << len(arr) # size of the subsets list
+    for i in range(size):
+        set = create_subset(arr, i)
+        subsets.append(set)
+    return subsets
+
+def create_subset(arr, i):
+    index = 0
+    set = []
+    while i:
+        if i & 1:
+            set.append(arr[index])
+        i = i >> 1
+        index += 1
+    return set
+
+power_set([1,2,3,4])
