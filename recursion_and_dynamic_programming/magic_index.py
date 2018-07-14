7264a86326eb74bcd62184035e9d639895928497
Ben Arbib <ba2490@columbia.edu>
2018-07-14 16:21:25 -0500

    checks arrays with dups for a magic index

--- recursion_and_dynamic_programming/magic_index.py
+++ recursion_and_dynamic_programming/magic_index.py
@@ -3,7 +3,8 @@
 # index
 
 def find_magic_index(arr):
-    return find_helper(arr, 0, len(arr)-1)
+    print("checks without dups: {}".format(find_helper(arr, 0, len(arr)-1)))
+    print("checks with dups: {}".format(find_magic_dups(arr, 0, len(arr)-1)))
 
 def find_helper(arr, l, r):
     if l>r:
@@ -17,8 +18,32 @@ def find_helper(arr, l, r):
         return find_helper(arr, l, mid-1)
     return find_helper(arr, mid+1, r)
 
+# there are duplicates in the array
+def find_magic_dups(arr, l, r):
+    if l>r:
+        return -1
+
+    mid = int((l+r)/2)
+
+    if arr[mid] == mid:
+        return mid
+
+    new_r = min(mid-1, arr[mid])
+    right = find_magic_dups(arr, l, new_r)
+    if  right > -1:
+        return right
+
+    new_l = max(mid+1, arr[mid])
+    return find_magic_dups(arr, new_l, r)
+
+
 arr = [-7,0,1,2,4,7]
 bad = [2,3,4,5,6,7]
+dups = [-10, -5, 2, 2, 2, 3, 4, 7, 9, 12, 13]
 
-print("shold return 4: {}".format(find_magic_index(arr)))
-print("shold return -1: {}".format(find_magic_index(bad)))
+print("should return 4:")
+find_magic_index(arr)
+print("\nshould return -1:")
+find_magic_index(bad)
+print("\nsArray with Dups, should return 2 with dups and 7 without:")
+find_magic_index(dups)
