5e26df3ed2dd31397defe55800be1dfcce271f6f
Ben Arbib <ba2490@columbia.edu>
2018-07-15 16:04:44 -0500

    parens added

---
+++ recursion_and_dynamic_programming/parens.py
@@ -0,0 +1,21 @@
+# implement an algorithm to print all valid combinations of n pairs of parentheses
+# input: 3 output: ((())) ()(()) (())() ()()() (()())
+
+def parens(n):
+    if n == 0:
+        return None
+    if n == 1:
+        return ["()"]
+
+    prev_parens = parens(n-1)
+    new_parens = set()
+    for p in prev_parens:
+        for i in range(len(p)):
+            if p[i] == '(':
+                temp = p[:i+1] + "()" + p[i+1:]
+                new_parens.add(temp)
+        new_parens.add("()"+p)
+
+    return new_parens
+
+print(parens(3))
