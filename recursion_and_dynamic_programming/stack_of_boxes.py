fedef217abddd408d9e66928029868ac6d8dedf2
BenIBM <ba2490@columbia.edu>
2018-08-02 10:53:40 -0500

    restarting stack of boxes

--- recursion_and_dynamic_programming/stack_of_boxes.py
+++ recursion_and_dynamic_programming/stack_of_boxes.py
@@ -5,65 +5,6 @@
 # of the heights of each box
 import random
 
-###################################
-#           recursoin             #
-###################################
-def tallestBoxStack(listOfBoxes):
-    last = len(listOfBoxes)-1
-    return tallestBoxStackHelper(listOfBoxes, last, None)
-
-def tallestBoxStackHelper(l, i, prevBox):
-    if i<0:
-        return 0
-    currentBox = l[i]
-    if i==0:
-        return currentBox.height
-
-    if prevBox and (prevBox.width < currentBox.width or prevBox.depth < currentBox.depth):
-        return 0
-
-    nextBox = l[i-1]
-    if currentBox.width > nextBox.width and currentBox.depth > nextBox.depth:
-        print(currentBox.height)
-        return currentBox.height + tallestBoxStackHelper(l, i-1, currentBox)
-
-
-    with_current = currentBox.height + tallestBoxStackHelper(l, i-2, currentBox)
-    without_curr = tallestBoxStackHelper(l, i-1, prevBox)
-
-    return max(with_current, without_curr, currentBox.height)
-
-###################################
-#         memoization             #
-###################################
-def tallestBoxStackMem(boxes):
-    last = len(boxes)-1
-    arr = [-1 for _ in range(len(boxes))]
-    print(arr)
-    i = 0;
-    return helper(boxes, arr, i)
-
-def helper(boxes, arr, i):
-    if i<0:
-        return 0
-    if arr[i] != -1:
-        return arr[i]
-    if i == 0:
-        arr[i] = boxes[i].height
-        return arr[i]
-
-    curr = boxes[i]
-    nextBox = boxes[i-1]
-
-    if curr.width > nextBox.width and curr.depth > nextBox.depth:
-        arr[i] = curr.height + tallestBoxStackHelper(l, i-1)
-    else:
-        with_current = curr.height + tallestBoxStackHelper(l, i-2)
-        without_curr = tallestBoxStackHelper(l, i-1)
-        arr[i] = max(with_current, without_curr)
-
-    return arr[i]
-
 class Box:
     def __init__(self, height, width, depth):
         self.height = height
@@ -107,5 +48,5 @@ def create_list_of_boxes(n):
 # test
 boxes = sorted(create_list_of_boxes(10))
 print(boxes)
-print("regular recursion: {}".format(tallestBoxStack(boxes)))
+#print("regular recursion: {}".format(tallestBoxStack(boxes)))
 print("memoization: {}".format(tallestBoxStackMem(boxes)))
